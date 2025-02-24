13d2fca56874e7666c15498ff62e01c24481d60e
Junyi Chen <chenjunyi@sensetime.com>
2025-02-24 17:56:35 +0800

    save

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1 \
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6 \
 python3 -m sglang.launch_server --model-path /mnt/nvme0/models/Meta-Llama-3-8B \
     --port 9999 --disable-radix-cache --enable-mixed-chunk \
     --disable-overlap-schedule --enable-custom-scheduler
