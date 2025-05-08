930b1ac37a0db09a644779aed9d7d029c80845f2
s7a9 <dch_7723@outlook.com>
2025-05-08 10:21:35 +0800

    save

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -1109,7 +1109,8 @@ class Scheduler:
         if not batch.check_decode_mem(self.decode_mem_cache_buf_multiplier) or (
             test_retract and batch.batch_size() > 10
         ):
-            assert False, f"Decode out of memory happened. {batch.batch_size()=}"
+            if self.__class__.__name__ == "MyScheduler":
+                assert False, f"Decode out of memory happened. {batch.batch_size()=}"
             old_ratio = self.new_token_ratio
 
             retracted_reqs, new_token_ratio = batch.retract_decode()
